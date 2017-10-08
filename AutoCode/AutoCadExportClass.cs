a7a1b72c3fb196cbfbd25d38f9b826f6013c0363
luistreml <luistreml@aol.com>
2017-10-08 19:57:29 +0200

    test

---
+++ AutoCode/AutoCadExportClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCode
+{
+    class AutoCadExportClass
+    {
+    }
+
+    //AutoCad öffnen
+    //Zeichnung öffnen
+    //Blöcke durchsuchen
+}
---
+++ AutoCode/EAs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCode
+{
+    class EAclass
+    {
+        public string symbol { get; set; }
+        public string adress { get; set; }
+    }
+}
